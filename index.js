184f5a69b565917127a56bc5191009b0db59e6eb
ittehad-abdullah <ittehad15-3530@diu.edu.bd>
2022-09-04 02:18:16 +0600

    spinner done

--- index.js
+++ index.js
@@ -1,55 +1,58 @@
 const categories = () => {
-    fetch('https://openapi.programming-hero.com/api/news/categories')
+  fetch('https://openapi.programming-hero.com/api/news/categories')
     .then(res => res.json())
     .then(data => displayCategories(data.data.news_category))
     .catch(error => console.log(error))
 }
 
 const displayCategories = data => {
-    // console.log(data);
-    const categoryLinks = document.getElementById('category-links');
+  // console.log(data);
+  const categoryLinks = document.getElementById('category-links');
 
-    data.forEach(category => {
-        // console.log(category.category_id);
-        const li = document.createElement('li');
-        li.classList.add('nav-item');
+  data.forEach(category => {
+    // console.log(category.category_id);
+    const li = document.createElement('li');
+    li.classList.add('nav-item');
 
-        li.innerHTML = `
+    li.innerHTML = `
         <a onclick = "newsByCategorie('${category.category_id}')" class="nav-link" href="#">${category.category_name}</a>
         `
-        categoryLinks.appendChild(li);
-    });
+    categoryLinks.appendChild(li);
+  });
 }
 categories();
 
 const newsByCategorie = (id) => {
-    const url = `https://openapi.programming-hero.com/api/news/category/${id}`
-    // console.log("url:", url)
-    fetch(url)
+
+  const spinner = document.getElementById("spinner");
+  spinner.classList.remove("d-none");
+
+  const url = `https://openapi.programming-hero.com/api/news/category/${id}`
+  // console.log("url:", url)
+  fetch(url)
     .then(res => res.json())
     .then(data => displayNews(data.data))
     .catch(error => console.log(error))
 }
 
 const displayNews = (allNews) => {
-    // console.log(allNews);
-    const newsContainer = document.getElementById('news-container');
-    for (const news of allNews){
-        // console.log(news);
-    }
-    allNews.forEach(news => {
-        const div = document.createElement('div');
-        div.classList.add('class="row"');
-        div.classList.add('class="g-0"');
+  // console.log(allNews);
+  const newsContainer = document.getElementById('news-container');
+  newsContainer.textContent = " ";
 
-        div.innerHTML = `
+  allNews.forEach(news => {
+    const div = document.createElement('div');
+    div.classList.add('class="row"');
+    div.classList.add('class="g-0"');
+
+    div.innerHTML = `
         <div class="col-md-4">
                     <img src="${news.thumbnail_url}" class="img-fluid rounded-start" alt="...">
                 </div>
                 <div class="col-md-8">
                     <div class="card-body">
                         <h5 class="card-title">${news.title}</h5>
-                        <p style="text-overflow: ellipsis;" class="card-text">${news.details.length > 100 ? news.details.slice(0,100) + '...' : news.details}</p>
+                        <p style="text-overflow: ellipsis;" class="card-text">${news.details.length > 100 ? news.details.slice(0, 100) + '...' : news.details}</p>
                         <p class="card-text"><small class="text-muted">
                         <img src="${news.author.img}" class="img-fluid rounded-start" alt="...">
                         ${news.author.name}
@@ -75,7 +78,7 @@ const displayNews = (allNews) => {
       </div>
       <div class="modal-body">
         <p>Total View: ${news.total_view ? news.total_view : "No data available"}</p>
-        <p>Author Name: ${news.author.name ? news.author.name: "No data available"}</p>
+        <p>Author Name: ${news.author.name ? news.author.name : "No data available"}</p>
         <p>Published date: ${news.author.published_date ? news.author.published_date : "No data available"}</p>
       </div>
       <div class="modal-footer">
@@ -89,7 +92,9 @@ const displayNews = (allNews) => {
                 </div>
                 <br>
         `;
-        newsContainer.appendChild(div);
-    });
+    newsContainer.appendChild(div);
+
+    spinner.classList.add("d-none");
+  });
 }
 
